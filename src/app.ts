98b8bba7db8569364ada9fc29f9414635150109a
Alexmaingi <alexmaingi077@gmail.com>
2023-04-28 11:24:47 +0300

    changes

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+interface streak {
+  id: number;
+  streakTask: string;
+  time: number;
+}
+
+class Streake {
+  async ShowStreak() {
+    const response = await fetch("http://localhost:3000/streak");
+    console.log(response);
+    const streakes = (await response.json()) as streak[];
+    console.log(streakes);
+
+    let html = "";
+    streakes.forEach((streaker) => {
+      console.log(streaker);
+      html += `
+       <div class="content-streak" key=${streaker.id}>
+       <h1>${streaker.streakTask}</h1>
+
+       <h2>Its been ${streaker.time}days</h2>
+     </div>
+  
+       `;
+    });
+    const app = document.querySelector(".streak-content")! as HTMLDivElement;
+    app.innerHTML = html;
+  }
+
+  static async addStreak() {
+    // const newPhoto= Photos.readValue()
+    // console.log(newPhoto);
+    const newStreak = { time: Date, streakTask: String };
+    await fetch("http://localhost:3000/streak", {
+      method: "POST",
+      body: JSON.stringify(newStreak),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
+new Streake().ShowStreak();
